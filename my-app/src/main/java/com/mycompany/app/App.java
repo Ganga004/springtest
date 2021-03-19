c2fff93af143bc59082ec36c11c571833d79c999
Ganga004 <43214309+Ganga004@users.noreply.github.com>
2021-03-19 19:23:44 +0530

    Update App.java

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "manisankar learning java" );
+        System.out.println( "bala learning java" );
     }
 }
