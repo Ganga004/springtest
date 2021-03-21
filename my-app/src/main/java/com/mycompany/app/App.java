c109d244d84cd01fa69a10e492fb3a4bebdfab8b
Ganga004 <ganga.tharan004@gmail.com>
2021-03-21 15:56:01 +0530

    ganga learning spring

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "bala learning java" );
+        System.out.println( "gangatharan learning SPring" );
     }
 }
